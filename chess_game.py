c70b5e7662e1f99e9d97a1f48a167e1b2e0922ef
Brian R Reed <b.r.reed@gmail.com>
2021-03-23 15:35:24 -0700

    adding starting functions in Game class

--- chess_game.py
+++ chess_game.py
@@ -1,5 +1,20 @@
-from chess import BoardState, Chess
+from chess import Chess
 
 class Game():
     def __init__(self):
+        pass
+
+    def start_game(self):
+        pass
+
+    def move(self, player, piece, c_coords, d_coords):
+        pass
+
+    def list_moves(self, player, piece, c_coords):
+        pass
+
+    def forfiet(self, player):
+        pass
+
+    def castle(self, player, dir):
         pass
