9b8ba8bd2ab277adc9943bf9ad186679abe45dc1
Brian R Reed <b.r.reed@gmail.com>
2021-05-04 17:00:41 -0700

    added move functionality in game class

--- game_flow.py
+++ game_flow.py
@@ -1,4 +1,4 @@
-from chess import Chess
+from game_logic import Chess
 
 class Game():
     def __init__(self):
@@ -9,9 +9,28 @@ class Game():
         self.black = player2
 
     def move(self, player, piece, c_coords, d_coords):
+        """allows player to move piece
+
+        Args:
+            player ([type]): [description]
+            piece ([type]): [description]
+            c_coords ([type]): [description]
+            d_coords ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         if self.get_piece(player, piece) not in c.current_state(c_coords):
             return False
-        
+        if not c.bs.check_coords(d_coords):
+            return False
+        if not c.bs.coords_valid(d_coords):
+            return False
+        if not c.bs.coords_not_equal(c_coords, d_coords):
+            return False
+        if c.bs.piece_movement(piece, c_coords, d_coords):
+            c.move_piece(piece, c_coords, d_coords)
+            return True
 
     def list_moves(self, player, piece, c_coords):
         """returns list of all possible moves for piece in c_coords"""
