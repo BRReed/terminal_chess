937365095b813976336cb6844561cdd5c0ed42f4
Brian R Reed <b.r.reed@gmail.com>
2022-02-01 19:00:59 -0800

    added create_account, sign_in shell funcs, changed origin sign_in to get_info. added logic to get_info to move user forward
    Signed-off-by: Brian R Reed <b.r.reed@gmail.com>

--- game_view.py
+++ game_view.py
@@ -5,7 +5,7 @@ def cleanup_ip(ipInfo):
     cleanIP = f"{ipInfo}"
     cleanIP = cleanIP.strip("['")
     cleanIP = cleanIP.split(" ")
-    sign_in(cleanIP[0])
+    get_info(cleanIP[0])
 
 
 def welcome_screen():
@@ -33,7 +33,8 @@ Welcome to
 
     """)
 
-def sign_in(userIP):
+
+def get_info(userIP):
     welcome_screen()
     print("""
 Please enter 1 to create an account, or 2 if you already have one.
@@ -42,17 +43,31 @@ Please enter 1 to create an account, or 2 if you already have one.
     while True:
         if i >= 10:
             exit_game(userIP, True, "Too many unsuccessful login attempts")
-            break
         try:
             sign_in_or_up = input(">")
             if sign_in_or_up not in (1, 2):
                 i += 1
                 print("You must enter the number '1' or the number '2'.")
                 continue
+            else:
+                break
         
         except:
             i += 1
             pass
+    if sign_in_or_up == 1:
+        create_account(userIP)
+    elif sign_in_or_up == 2:
+        sign_in(userIP)
+    else:
+        print("unknown error getting user input")
+        exit()
+
+def create_account(userIP):
+    pass
+
+def sign_in(userIP):
+    pass
 
 def exit_game(userIP, ban, reason="None"):
     """exits out of chess program
@@ -67,6 +82,7 @@ def exit_game(userIP, ban, reason="None"):
         banList['to_ban'].append({userIP: reason})
         write_to_json('ban_list.json', banList)
     exit()
+
 
 def get_json_info(fileName):
     """get information in a json file
