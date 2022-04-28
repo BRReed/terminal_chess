33015ea3d806ca5351fe6fa63eb1be5f43c361aa
Brian R Reed <b.r.reed@gmail.com>
2022-04-27 20:29:50 -0700

    removed resign func (functionality inherited by end_game). Changed call for resign to call for end_game
    Signed-off-by: Brian R Reed <b.r.reed@gmail.com>

--- game_view.py
+++ game_view.py
@@ -45,7 +45,7 @@ def main(ip_info):
             if user_input == 'back':
                 break
             if user_input in ["xx","resign"]: 
-                resign(uname, opponent, game_id)
+                end_game(opponent, uname, game_id)
                 print(f"You have resigned from {game_id} against {opponent}")
                 break
             input_valid, move, msg, = g.input_parse(is_black, user_turn, user_input)
@@ -441,32 +441,14 @@ def commands():
   `back`
 """)
 
-def resign(user, opponent, game_id):
-    """resign from an in progress game
-
-    Args:
-        user (str): name of user resigning
-        opponent (str): name of opponent in game being resigned
-        game_id (str): id of the game being resigned
-    """
-
-    user_data = get_json_info('users.json')
-    user_data[user]["currentgames"].remove(game_id)
-    user_data[opponent]["currentgames"].remove(game_id)
-    write_to_json('users.json', user_data)
-
-    games_data = get_json_info('currentgames.json')
-    games_data["in_progress"].pop(game_id)
-    write_to_json('currentgames.json', games_data)
-    # future: add win to opponent, loss to user
 
 def end_game(winner, loser, game_id):
     """resign from an in progress game
 
     Args:
-        user (str): name of user resigning
-        opponent (str): name of opponent in game being resigned
-        game_id (str): id of the game being resigned
+        winner (str): name of user who won the game
+        loser (str): name of user who lost the game
+        game_id (str): id of the game being removed
     """
     user_data = get_json_info('users.json')
     user_data[winner]["currentgames"].remove(game_id)
